7f5a4b6d0e20dc8b2577cce6155354e9c4d8ce93
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-08 11:04:26 +0530

    Volume of a Cylinder

---
+++ code5.java
@@ -0,0 +1,8 @@
+public class code5{
+	public static void main(String args[]){
+		double radius = 7;
+		double height = 9;
+		double volume = 3.14 * radius*radius *height;
+		System.out.println(volume);
+	}
+}
