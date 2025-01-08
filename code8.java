9481458ed5928d4131df70807079d09f64964e76
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-08 11:23:13 +0530

    Power calcuation

---
+++ code8.java
@@ -0,0 +1,8 @@
+public class code8{
+	public static void main(String args[]){
+		double base=10;
+		double exponent = 7;
+		double outcome = Math.pow(base,exponent);
+		System.out.println(outcome);
+	}
+}
